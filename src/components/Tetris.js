ab3029034df220db969cce8389d8157c1841a898
Boki23m <boki23usa@yahoo.com>
2020-04-25 23:12:02 -0700

    tetrominos added

---
+++ src/components/Tetris.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { createStage } from '../gameHelpers';
+// Components
+import Display from './Display';
+import Stage from './Stage';
+import StartButton from './StartButton';
+
+
+const Tetris = () => {
+
+    return (
+        <div>
+            <Stage stage={createStage()}/>
+            <aside>
+                <div>
+                    <Display text="Score" />
+                    <Display text="Rows" />
+                    <Display text="Level" />
+                </div>
+                <StartButton />
+            </aside>
+        </div>
+    );
+};
+
+export default Tetris;
